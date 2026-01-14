1a4e3fe3f48af97574d0c975ef9d7b26d820e898
Aaron Vogt <nyroxsystems@gmail.com>
2026-01-14 03:53:07 +0100

    Remove unused imports from CandlestickChart

--- dashboard/src/components/CandlestickChart.tsx
+++ dashboard/src/components/CandlestickChart.tsx
@@ -1,4 +1,4 @@
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Area, AreaChart } from 'recharts'
+import { XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Area, AreaChart } from 'recharts'
 
 interface CandlestickData {
     time: any
